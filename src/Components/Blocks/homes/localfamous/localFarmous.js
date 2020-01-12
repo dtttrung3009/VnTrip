11dc2538a6e0e88c1e58b9f24860d917d7b2753b
= <=>
2020-01-12 15:17:15 +0700

    add library

---
+++ src/Components/Blocks/homes/localfamous/localFarmous.js
@@ -0,0 +1,11 @@
+class localFarmous extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <>
+            </>
+         );
+    }
+}
+ 
+export default localFarmous;
